0969ff71b3b247ddf55a56546090086356d82311
Pratham Vadhulas <legacy.business18@gmail.com>
2023-03-07 22:34:03 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,24 +1,29 @@
 /* eslint-disable no-undef, no-unused-vars */
+let r = 4;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(600, 400);
   // Put setup code here
-  var center = windowWidth / 2;
 }
 
 function draw() {
-  // Put drawings here
-  var center = windowWidth / 2 - 125;
-  fill(234, 31, 81);
-  noStroke();
-  rect(center, 50, 250, 250);
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", center + 10, 250);
+  background(0);
+  translate(300, 200);
+  var center = width / (r * 2);
+  noFill();
+  stroke(252, 238, 33);
+  strokeWeight(8);
+  beginShape();
+  for (let i = 0; i < center + 1; i++) {
+    let angle = map(i, 0, center, 0, TWO_PI);
+    let y = map(sin(angle), -1, 1, -200, 200);
+    let x = map(i, 0, center + 1, -300, 300);
+    vertex(x, y);
+  }
+  endShape();
 }
 
 // This Redraws the Canvas when resized
-windowResized = function() {
-  resizeCanvas(windowWidth, windowHeight);
+windowResized = function () {
+  resizeCanvas(600, 400);
 };
