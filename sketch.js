534718b1ec128cb99c626c7369ec52fe6decd890
Pratham Vadhulas <legacy.business18@gmail.com>
2023-03-08 00:49:19 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,47 @@
-/* eslint-disable no-undef, no-unused-vars */
-let r = 4;
+let angleVel = 0.8;
+let angle = 0;
+
+let freqslider;
+
+let osc;
 
 function setup() {
-  createCanvas(600, 400);
-  // Put setup code here
+  createCanvas(800, 400);
+  freqslider = createSlider(80, 800, 120, 40);
+  freqslider.position(30, 20);
+  freqslider.style("width", "100px");
+
+  osc = new p5.Oscillator();
+  osc.setType("sine");
+  osc.freq(440); // Starting frequency
+  osc.amp(0.5); // Starting amplitude
+  osc.start();
 }
 
 function draw() {
-  background(0);
-  translate(300, 200);
-  var center = width / (r * 2);
+  background(220);
   noFill();
-  stroke(252, 238, 33);
-  strokeWeight(8);
+  // Draw the x-axis
+  line(20, height - 20, width - 20, height - 20);
+  // Draw the y-axis
+  line(20, 20, 20, height - 20);
+  line(10, height / 2, width - 20, height / 2);
+  let val = freqslider.value();
+
+  let amplitude = 100;
+  let frequency = val / 10000;
+  angleVel = val / 100;
+
+  freqVal = map(val, 80, 800, 200, 800);
+  osc.freq(freqVal);
+
   beginShape();
-  for (let i = 0; i < center + 1; i++) {
-    let angle = map(i, 0, center, 0, TWO_PI);
-    let y = map(sin(angle), -1, 1, -200, 200);
-    let x = map(i, 0, center + 1, -300, 300);
+  for (let x = 20; x < width - 20; x += 1) {
+    let y = height / 2 + amplitude * sin(frequency * (x + angle));
     vertex(x, y);
   }
   endShape();
-}
 
-// This Redraws the Canvas when resized
-windowResized = function () {
-  resizeCanvas(600, 400);
-};
+  // Update angle for animation
+  angle += angleVel;
+}
