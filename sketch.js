a4c3f1abf4e848d1679a0d3bf991401d957336ec
Pratham Vadhulas <legacy.business18@gmail.com>
2023-03-08 01:31:39 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,20 +2,33 @@ let angleVel = 0.8;
 let angle = 0;
 
 let freqslider;
-
+let volslider;
 let osc;
+
+let isOn = false;
+let muteToggle;
 
 function setup() {
   createCanvas(800, 400);
-  freqslider = createSlider(80, 800, 120, 40);
+  freqslider = createSlider(60, 800, 440, 20);
   freqslider.position(30, 20);
   freqslider.style("width", "100px");
+
+  let d = createDiv();
+  d.style("transform: rotate(-90deg)");
+  d.position(800, 80);
+  volslider = createSlider(0, 100, 80, 10);
+  volslider.style("height", "100px");
+  d.child(volslider);
 
   osc = new p5.Oscillator();
   osc.setType("sine");
   osc.freq(440); // Starting frequency
-  osc.amp(0.5); // Starting amplitude
-  osc.start();
+  osc.amp(0.8); // Starting amplitude
+
+  muteToggle = createButton("Play Audio");
+  muteToggle.position(820, 20);
+  muteToggle.mousePressed(handleMuteToggle);
 }
 
 function draw() {
@@ -26,14 +39,17 @@ function draw() {
   // Draw the y-axis
   line(20, 20, 20, height - 20);
   line(10, height / 2, width - 20, height / 2);
-  let val = freqslider.value();
 
-  let amplitude = 100;
+  let val = freqslider.value();
+  let vol = volslider.value();
+
+  let amplitude = map(vol, 0, 100, 0, 1) * 160;
   let frequency = val / 10000;
   angleVel = val / 100;
 
-  freqVal = map(val, 80, 800, 200, 800);
+  freqVal = map(val, 60, 800, 60, 800);
   osc.freq(freqVal);
+  osc.amp(map(vol, 0, 100, 0, 1));
 
   beginShape();
   for (let x = 20; x < width - 20; x += 1) {
@@ -44,4 +60,16 @@ function draw() {
 
   // Update angle for animation
   angle += angleVel;
+}
+
+function handleMuteToggle() {
+  if (isOn == true) {
+    osc.stop();
+    isOn = false;
+    muteToggle.html("Unmute");
+  } else {
+    osc.start();
+    isOn = true;
+    muteToggle.html("Mute");
+  }
 }
